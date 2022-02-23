a9d74709a2485d4dcad88de89c9477f6b22e505a
awaisanwar544 <awaisanwar544@gmail.com>
2022-02-23 21:15:05 +0500

    Add test for TaskList

--- src/tests/taskList.test.js
+++ src/tests/taskList.test.js
@@ -1,3 +1,24 @@
-test('works', () => {
+import TaskList from '../modules/taskList.js';
 
+describe('addNew method', () => {
+  const list = new TaskList();
+  const item1 = {
+    index: 1,
+    desc: 'test description',
+    completed: false,
+  };
+  const item2 = {
+    index: 2,
+    desc: 'test description 2',
+    completed: false,
+  };
+  test('updates the length to 1 of storage in TaskList', () => {
+    list.addNew(item1);
+    expect(list.storage).toHaveLength(1);
+  });
+
+  test('updates the length to 2 of storage in TaskList', () => {
+    list.addNew(item2);
+    expect(list.storage).toHaveLength(2);
+  });
 });
