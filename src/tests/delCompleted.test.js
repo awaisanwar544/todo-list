073478be3cac5f24465fd3221f6e65bc9f002e7f
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-02-24 20:30:09 +0430

    check type is output is array

--- src/tests/delCompleted.test.js
+++ src/tests/delCompleted.test.js
@@ -22,19 +22,28 @@ describe('Deleted completed tasks', () => {
     storage: arr,
   };
 
+  const result = [
+    {
+      index: 1,
+      desc: 'Initial task',
+      completed: false,
+    },
+    {
+      index: 3,
+      desc: 'third task',
+      completed: false,
+    },
+  ];
+
   test('Removs items with completed: true', () => {
-    const result = [
-      {
-        index: 1,
-        desc: 'Initial task',
-        completed: false,
-      },
-      {
-        index: 3,
-        desc: 'third task',
-        completed: false,
-      },
-    ];
     expect(delCompleted(list)).toEqual(result);
+  });
+
+  test('check type input is object', () => {
+    expect(typeof list).toBe('object');
+  });
+
+  test('check type is output is array', () => {
+    expect(Array.isArray(result)).toBe(true);
   });
 });
